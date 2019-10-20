787a018c9f7af973156fcaaf798d736633718230
ArmenianDavid <imast.un@mail.ru>
2019-10-20 14:35:15 +0400

    Updates

---
+++ precache-manifest.7062e9c6f0c9283b3b7062d696a17afe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e76075c3cfb07a7967733251fb90491",
+    "url": "https.//ArmenianDavid.github.io/project2/index.html"
+  },
+  {
+    "revision": "4eb3572b063066849c82",
+    "url": "https.//ArmenianDavid.github.io/project2/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d607a7c0405f489a9a99",
+    "url": "https.//ArmenianDavid.github.io/project2/static/js/2.62c99d41.chunk.js"
+  },
+  {
+    "revision": "4eb3572b063066849c82",
+    "url": "https.//ArmenianDavid.github.io/project2/static/js/main.ee8bb6e7.chunk.js"
+  },
+  {
+    "revision": "fff075120ccf73579bbb",
+    "url": "https.//ArmenianDavid.github.io/project2/static/js/runtime-main.07c096dc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "https.//ArmenianDavid.github.io/project2/static/media/logo.25bf045c.svg"
+  }
+]);
